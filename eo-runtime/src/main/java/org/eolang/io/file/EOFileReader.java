fae09f9168647d8748da32c01cd9e8570ac63387
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2021-05-24 22:00:40 +0300

    added basic file reading and writing features

---
+++ eo-runtime/src/main/java/org/eolang/io/file/EOFileReader.java
@@ -0,0 +1,17 @@
+package org.eolang.io.file;
+
+import org.eolang.EOstring;
+
+/**
+ * @author <a href="mailto:shankhadeepghoshal1996@gmail.com">Shankhadeep Ghoshal</a>
+ * @since 0.1.25
+ **/
+
+public class EOFileIReader extends EOFile {
+
+    public EOFileIReader(EOstring path) {
+        super(path);
+    }
+
+
+}
---
+++ eo-runtime/src/main/java/org/eolang/io/file/EOFileWriter.java
@@ -0,0 +1,7 @@
+package org.eolang.io.file;/**
+* @since
+* @author <a href="mailto:shankhadeepghoshal1996@gmail.com">Shankhadeep Ghoshal</a>
+**/
+    
+ public class EOFileWriter {
+}
