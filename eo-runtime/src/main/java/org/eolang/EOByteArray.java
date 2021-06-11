0452e550c33c20cd308f83eec57c4d4190059cae
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2021-06-11 22:01:37 +0300

    Added byte array wrapper class for internal use.

--- eo-runtime/src/main/java/org/eolang/EOByteArray.java
+++ eo-runtime/src/main/java/org/eolang/EOByteArray.java
@@ -1,19 +1,57 @@
 package org.eolang;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import org.eolang.core.EOObject;
+import org.eolang.core.data.EOData;
 
-/***
- * Represents a byte type
+/**
+ * Represents a byte array type
+ *
  * @version %I%, %G%
  */
-public class EObyte extends EOObject {
-    private final byte byteValue;
+public class EOByteArray extends EOObject {
 
-    public EObyte(EOint integer) {
-        byteValue = integer._getData().toInt().byteValue();
+    private byte[] byteArray;
+
+    public EOByteArray() {
+        this.byteArray = null;
     }
 
-    public EObyte(EOchar characterVal) {
-        byteValue = (byte) characterVal._getData().toChar().charValue();
+    public EOByteArray getBytesFromString(final EOstring string, final EOstring charsetString) {
+        final var charset = Charset.forName(charsetString._getData().toString());
+        this.byteArray = string._getData().toString().getBytes(charset);
+        return this;
+    }
+
+    public EOstring getStringFromByes(final EOByteArray eoByteArray, EOstring charsetString) {
+        final var data = getUnderlyingByteArrayOfEoByteArrayObj(eoByteArray);
+        final var charset = Charset.forName(charsetString._getData().toString());
+        return new EOstring(new String(data, charset));
+    }
+
+    public EOByteArray getBytesFromInt(final EOint integer) {
+        final var longVal = integer._getData().toInt();
+        final var buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(longVal);
+        this.byteArray = buffer.array();
+        return this;
+    }
+
+    public EOint getIntFromBytes(final EOByteArray eoByteArray) {
+        final var buffer = ByteBuffer.allocate(Long.BYTES);
+        final var bytes = getUnderlyingByteArrayOfEoByteArrayObj(eoByteArray);
+        buffer.put(bytes, 0, bytes.length);
+        buffer.flip();
+        return new EOint(buffer.getLong());
+    }
+
+    public byte[] getUnderlyingByteArrayOfEoByteArrayObj(final EOByteArray byteArray) {
+        return (byte[]) byteArray._getData().toObject();
+    }
+
+    @Override
+    public EOData _getData() {
+        return new EOData(byteArray);
     }
 }
